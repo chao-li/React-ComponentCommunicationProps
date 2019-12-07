24ecb8f74647be832de79da570c1085409f2626f
Chao <chaoli@chaos-mbp.home>
2019-12-08 02:48:50 +1100

    Component Reuse - Implement Approval Card

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import faker from 'faker'; // creates fake data for dev
+//import faker from 'faker'; // creates fake data for dev
 
 const CommentDetail = (props) => {
     return (
         <div className = "comment">
-                <a href = "/" className = "avatar">
-                    <img alt = "avatar" src={props.portrait}/>
+            <a href = "/" className = "avatar">
+                <img alt = "avatar" src={props.portrait}/>
+            </a>
+            <div className="content">
+                <a href="/" className="author">
+                    {props.author} 
                 </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        {props.author} 
-                    </a>
-                    <div className = "metadata">
-                        <span className = "date">
-                            {props.timeAgo}
-                        </span>
-                    </div>
-                    <div className="text">
-                        {props.commentText}
-                    </div>
+                <div className = "metadata">
+                    <span className = "date">
+                        {props.timeAgo}
+                    </span>
+                </div>
+                <div className="text">
+                    {props.commentText}
                 </div>
             </div>
+        </div>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,25 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail'; // don't need to end with .js
+import ApprovalCard from './ApprovalCard';
 
 
 const App = () => {
     return (
         <div className = "ui container comments">
-            <CommentDetail portrait = {faker.image.avatar()}
-                            author = "Sam" 
-                            timeAgo = "Today at 4:45PM"
-                            commentText = "Hi, what's up?"
+            <ApprovalCard/>
+            <CommentDetail 
+                portrait = {faker.image.avatar()}
+                author = "Sam" 
+                timeAgo = "Today at 4:45PM"
+                commentText = "Hi, what's up?"
                             />
-            <CommentDetail portrait = {faker.image.avatar()}
-                            author = "Alex" 
-                            timeAgo = "Today at 2:00AM"
-                            commentText = "What a nice day!"
+            <CommentDetail 
+                portrait = {faker.image.avatar()}
+                author = "Alex" 
+                timeAgo = "Today at 2:00AM"
+                commentText = "What a nice day!"
                             />
-            <CommentDetail portrait = {faker.image.avatar()}
-                            author = "Jane" 
-                            timeAgo = "Yesterday at 5:00PM"
-                            commentText = "Going to dinner!"
+            <CommentDetail 
+                portrait = {faker.image.avatar()}
+                author = "Jane" 
+                timeAgo = "Yesterday at 5:00PM"
+                commentText = "Going to dinner!"
                             />
 
         </div>
